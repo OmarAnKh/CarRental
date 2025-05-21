c14106cce4da6041f8662c745e2685729d884c6c
Omar Khalili <OmarKhalili810@gmail.com>
2025-05-21 11:14:11 +0300

    fixing the system .env

---
+++ CarRental/Services/EmailSender.cs
@@ -0,0 +1,14 @@
+//
+// using CarRental.Services.Interfaces;
+//
+// namespace CarRental.Services;
+//
+// public class EmailSender : IEmailSender
+// {
+//
+//     public Task SendEmailAsync(string email, string subject, string message)
+//     {
+//         var mail = "omarkhalili810@gmail.com";
+//         var pw = "";
+//     }
+// }
